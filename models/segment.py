3b5d952f513fcb1a7c59bbe76258d0e43b198ca8
Aaron <161750474+aaidoudi91@users.noreply.github.com>
2026-03-10 13:26:26 +0100

    Classe Segment pour Skyline

---
+++ models/segment.py
@@ -0,0 +1,14 @@
+class Segment:
+    """ Représente un segment horizontal de la skyline.
+    Un segment (x, largeur, hauteur) signifie que la zone [x, x+largeur[ est remplie jusqu'à 'hauteur'. """
+
+    # Empêche la création d'un dictionnaire par instance pour économiser la RAM lors du DFS.
+    __slots__ = ('x', 'largeur', 'hauteur')
+
+    def __init__(self, x, largeur, hauteur):
+        self.x = x
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def x_fin(self):
+        return self.x + self.largeur
